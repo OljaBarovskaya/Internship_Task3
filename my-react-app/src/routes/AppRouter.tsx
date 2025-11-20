72929a38bb044e18dce060221b1ef6b1dd517cec
Olga Barovskaya <asdfg1234_08@mail.ru>
2025-11-20 18:47:16 +0300

    style: nav links

--- my-react-app/src/routes/AppRouter.tsx
+++ my-react-app/src/routes/AppRouter.tsx
@@ -1,17 +1,32 @@
 import { BrowserRouter } from "react-router-dom";
-import { Route, Routes, Link } from "react-router-dom";
+import { Route, Routes, NavLink } from "react-router-dom";
 import WeatherDashboard from "../components/WeatherDashboard";
 import Common from "../components/Common";
 import Contacts from "../components/Contacts";
+
+interface MyComponentProps {
+  isActive: boolean;
+}
+
+const navLinkStyle = ({ isActive }: MyComponentProps) => ({
+  color: isActive ? "#007bff" : "#333",
+  fontWeight: isActive ? "bold" : "normal",
+});
 
 export default function AppRouter() {
   return (
     <BrowserRouter>
       <header>
         <nav>
-          <Link to="/">Weather</Link>
-          <Link to="/common">Common</Link>
-          <Link to="/contacts">Contacts</Link>
+          <NavLink to="/" style={navLinkStyle as React.CSSProperties}>
+            Weather
+          </NavLink>
+          <NavLink to="/common" style={navLinkStyle as React.CSSProperties}>
+            Common
+          </NavLink>
+          <NavLink to="/contacts" style={navLinkStyle as React.CSSProperties}>
+            Contacts
+          </NavLink>
         </nav>
       </header>
       <main>

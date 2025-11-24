4661856dbdb1573e5fa5b09cf7ab3cb248247201
Olga Barovskaya <asdfg1234_08@mail.ru>
2025-11-24 14:34:34 +0300

    feat: imolement DetailsBlock and LocationBlock

---
+++ my-react-app/src/components/DetailsBlock.tsx
@@ -0,0 +1,25 @@
+import BlockRow1 from "./BlockRow1";
+import BlockRow2 from "./BlockRow2";
+
+export default function DetailsBlock({
+  name,
+  value,
+}: {
+  name: string;
+  value: string;
+}) {
+  const nameL = name.toLowerCase();
+  return (
+    <div className="w-[157px] h-[134px] p-[16px] rounded-[16px] bg-[#00008B] flex flex-col gap-y-[16px]">
+      <BlockRow1>
+        <img
+          className="w-[24px]"
+          src={`./src/assets/img/${nameL}.png`}
+          alt={name}
+        ></img>
+        <h3 className="inline-block">{name}</h3>
+      </BlockRow1>
+      <BlockRow2>{value}</BlockRow2>
+    </div>
+  );
+}
---
+++ my-react-app/src/components/LocationDetailsBlock.tsx
@@ -0,0 +1,38 @@
+import DashboardBlock from "./DashboardBlock";
+import BlockRow1 from "./BlockRow1";
+import BlockRow2 from "./BlockRow2";
+import DetailsBlock from "./DetailsBlock";
+import { currentWeatherData } from "../data";
+import { useWeather } from "../contents/Contents";
+import convertTime from "../utils/helpers";
+
+export default function LocationDetailsBlock() {
+  const weatherData = useWeather();
+  const humidity = weatherData?.weather?.main.humidity;
+  const pressure = weatherData?.weather?.main.pressure;
+  const wind = weatherData?.weather?.wind.speed;
+  const timezone = weatherData?.weather?.timezone;
+  const sunrise = convertTime(
+    weatherData?.weather?.sys.sunrise! * 1000 + timezone!
+  );
+  const sunset = convertTime(
+    weatherData?.weather?.sys.sunset! * 1000 + timezone!
+  );
+  const visibility = weatherData?.weather?.visibility;
+
+  return (
+    <DashboardBlock>
+      <BlockRow1>
+        <h2>Detailed Weather</h2>
+      </BlockRow1>
+      <BlockRow2 addStyle="flex-wrap gap-y-[18px] gap-x-[18px]">
+        <DetailsBlock name="Humidity" value={humidity + "%"} />
+        <DetailsBlock name="Pressure" value={pressure + " " + "hPa"} />
+        <DetailsBlock name="Wind" value={wind + " " + "m/s"} />
+        <DetailsBlock name="Visibility" value={visibility + " " + "m"} />
+        <DetailsBlock name="Sunrise" value={sunrise} />
+        <DetailsBlock name="Sunset" value={sunset} />
+      </BlockRow2>
+    </DashboardBlock>
+  );
+}

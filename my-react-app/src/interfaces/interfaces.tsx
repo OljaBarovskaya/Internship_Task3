431658e05119132e8dc37fb9e4ae05cc4f5709c1
Olga Barovskaya <asdfg1234_08@mail.ru>
2025-11-24 14:42:10 +0300

    feat: create interfaces

---
+++ my-react-app/src/interfaces/interfaces.tsx
@@ -0,0 +1,9 @@
+export interface StyledCompWithChildren {
+  children?: React.ReactNode;
+  addStyle?: string;
+}
+
+export interface WeatherProviderType {
+  children: React.ReactNode;
+  city: string;
+}

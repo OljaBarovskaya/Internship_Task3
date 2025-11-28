c273ecc08cf51d2416b2a859f5ddd26fb056e723
Olga Barovskaya <asdfg1234_08@mail.ru>
2025-11-28 16:07:39 +0300

    refactor: create images imports

--- my-react-app/src/components/weatherPage/DetailsBlock.tsx
+++ my-react-app/src/components/weatherPage/DetailsBlock.tsx
@@ -1,5 +1,11 @@
 import BlockRow1 from "../../containers/BlockRow1";
 import BlockRow2 from "../../containers/BlockRow2";
+import Humidity from "../../assets/img/humidity.png";
+import Pressure from "../../assets/img/pressure.png";
+import Sunrise from "../../assets/img/sunrise.png";
+import Sunset from "../../assets/img/sunset.png";
+import Visibility from "../../assets/img/visibility.png";
+import Wind from "../../assets/img/wind.png";
 
 export default function DetailsBlock({
   name,
@@ -8,15 +14,37 @@ export default function DetailsBlock({
   name: string;
   value: string;
 }) {
-  const nameL = name.toLowerCase();
+  let iconImg;
+
+  function chooseIcon() {
+    switch (name) {
+      case "Humidity":
+        iconImg = Humidity;
+        break;
+      case "Pressure":
+        iconImg = Pressure;
+        break;
+      case "Sunset":
+        iconImg = Sunset;
+        break;
+      case "Sunrise":
+        iconImg = Sunrise;
+        break;
+      case "Visibility":
+        iconImg = Visibility;
+        break;
+      case "Wind":
+        iconImg = Wind;
+        break;
+    }
+  }
+
+  chooseIcon();
+
   return (
     <div className="w-[157px] h-[134px] p-[16px] rounded-[16px] bg-[#00008B] flex flex-col gap-y-[24px]">
       <BlockRow1>
-        <img
-          className="w-[24px]"
-          src={`./src/assets/img/${nameL}.png`}
-          alt={name}
-        ></img>
+        <img className="w-[24px]" src={iconImg} alt={name}></img>
         <h3 className="inline-block">{name}</h3>
       </BlockRow1>
       <BlockRow2 addStyle="text-[2em]">{value}</BlockRow2>
--- my-react-app/src/components/weatherPage/Location.tsx
+++ my-react-app/src/components/weatherPage/Location.tsx
@@ -1,3 +1,5 @@
+import LocationIcon from "../../assets/img/location.png";
+
 export default function Location({
   city,
   country,
@@ -7,7 +9,9 @@ export default function Location({
 }) {
   return (
     <div className="flex items-center p-[9px_0] gap-x-[8px] text-[1.8em]">
-      <div className="bg-[url('./src/assets/img/location.png')] w-[24px] h-[24px] inline-block"></div>
+      <div
+        className={`bg-[url('${LocationIcon}')] w-[24px] h-[24px] inline-block`}
+      ></div>
       {city + ", " + country}
     </div>
   );

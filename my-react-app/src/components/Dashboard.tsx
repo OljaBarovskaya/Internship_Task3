74de683e0aff3a3f3b8e2280837fdb6ce132806f
Olga Barovskaya <asdfg1234_08@mail.ru>
2025-11-28 11:12:26 +0300

    style: change column-2-layout style

--- my-react-app/src/components/Dashboard.tsx
+++ my-react-app/src/components/Dashboard.tsx
@@ -21,7 +21,7 @@ export default function Dashboard({ units }: { units: "metric" | "imperial" }) {
       }}
     >
       <div className="Dashboard w-full h-full text-[#FFFFFF] pt-[24px] pb-[24px]">
-        <div className="column-2-layout flex gap-x-[24px]">
+        <div className="column-2-layout flex gap-x-[24px] flex-wrap">
           <div className="w-[calc(50%-12px)] flex flex-col gap-y-[24px]">
             <LocationMainBlock units={units} />
             <LocationDetailsBlock units={units} />

4736d0ecc018dc24d48d499a33876dd526d4ff10
Olga Barovskaya <asdfg1234_08@mail.ru>
2025-11-28 14:23:12 +0300

    fix: made changes to vite.config

--- my-react-app/vite.config.ts
+++ my-react-app/vite.config.ts
@@ -3,6 +3,8 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import svgr from "vite-plugin-svgr";
 
+const repoName = "/my-react-app/";
+
 export default defineConfig({
   plugins: [
     react({
@@ -10,9 +12,11 @@ export default defineConfig({
         plugins: [["babel-plugin-react-compiler"]],
       },
     }),
+
     tailwindcss(),
     svgr(),
   ],
+  base: repoName,
   test: {
     globals: true,
     setupFiles: ["setupTests.ts"],

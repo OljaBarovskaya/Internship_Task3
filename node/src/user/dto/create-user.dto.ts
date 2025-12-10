c7a035ff0f798c2434adefd8b69966b0da9e29ce
Olga Barovskaya <asdfg1234_08@mail.ru>
2025-12-10 13:45:15 +0300

    feat: add validation to user.dto

--- node/src/user/dto/create-user.dto.ts
+++ node/src/user/dto/create-user.dto.ts
@@ -1,10 +1,12 @@
-import { IsNotEmpty, IsString } from "class-validator";
+import { IsNotEmpty, IsNumber, IsString, MaxLength } from "class-validator";
 
 export class CreateUserDto {
-  @IsString({ message: "Name is not a string" })
+  @IsString({ message: "Name must be of a string type" })
   @IsNotEmpty({ message: "Name field can't be empty" })
+  @MaxLength(50, { message: "Name must not be more than 50 characters" })
   userName: string;
 
   @IsNotEmpty({ message: "Password field can't be empty" })
+  @MaxLength(50, { message: "Password must not be more than 50 characters" })
   password: string;
 }

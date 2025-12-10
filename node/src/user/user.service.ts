cb5bb12c5224eaa370db82ffc2fc657804bbbeae
Olga Barovskaya <asdfg1234_08@mail.ru>
2025-12-10 13:44:42 +0300

    feat: add  user test and entity files

--- node/src/user/user.service.ts
+++ node/src/user/user.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from "@nestjs/common";
 import { CreateUserDto } from "./dto/create-user.dto";
 import { UpdateUserDto } from "./dto/update-user.dto";
-import { User } from "./entities/registration.entity";
+import { User } from "./entities/user.entity";
 import { InjectModel } from "@nestjs/mongoose";
 import { Model } from "mongoose";
 

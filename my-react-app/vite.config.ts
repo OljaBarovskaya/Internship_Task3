087cc57bd25ebf984e43931b2a17be1e220d5be2
Olga Barovskaya <asdfg1234_08@mail.ru>
2025-12-15 11:55:55 +0300

    init: add plugin to set absolute paths

--- my-react-app/vite.config.ts
+++ my-react-app/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import svgr from "vite-plugin-svgr";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [
@@ -13,7 +14,11 @@ export default defineConfig({
 
     tailwindcss(),
     svgr(),
+    tsconfigPaths({
+      root: process.cwd(),
+    }),
   ],
+
   base: "/Internship_Task3/",
   test: {
     globals: true,

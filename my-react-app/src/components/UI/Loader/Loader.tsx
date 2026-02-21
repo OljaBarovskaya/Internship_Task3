1b14ca7bfa1efec184521cac4609406dee257e61
Olga Barovskaya <asdfg1234_08@mail.ru>
2026-02-21 22:00:42 +0300

    fix: Loader

--- my-react-app/src/components/UI/Loader/Loader.tsx
+++ my-react-app/src/components/UI/Loader/Loader.tsx
@@ -1,9 +1,15 @@
 import * as S from "./Loader.styled";
 
-export function Loader({ height }: { height?: string }) {
+export function Loader({
+  height,
+  color = "white",
+}: {
+  height?: string;
+  color?: string;
+}) {
   return (
     <S.LoaderContainer className={height}>
-      <div className="loader-bbl" />
+      <div className={`loader-bbl text-${color}`} />
     </S.LoaderContainer>
   );
 }
